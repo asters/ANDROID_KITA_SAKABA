93c6146bb371ef5d38eb38e410f1e26a81755ee4
asters <kaonach31@yahoo.co.jp>
2014-10-11 17:01:07 +0900

    Revert "Revert "初期コミット""
    This reverts commit 69fa8553d8c7ed51f0caf2ab937725947e0d4847.

---
+++ SleepingBesideGirl/src/com/example/sleepingbesidegirl/Constants.java
@@ -0,0 +1,8 @@
+package com.example.sleepingbesidegirl;
+
+public class Constants {
+	public static final String SEMI_COLON = ":";
+	public static final String CURRENT_TIME_LABEL = "現在時刻";
+	public static final String ALARM_LABEL = "アラーム設定";
+	public static final String ALARM = "Alarm";
+}
